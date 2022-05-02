3dfa73e0d315d05965979aef4f5d4301c6ddc400
qLigature <imbrued.mitre@gmail.com>
2022-05-02 11:43:10 +0800

    added typewriter effect

---
+++ resources/js/index.js
@@ -0,0 +1,37 @@
+const Typewriter = window.Typewriter;
+const introTagline = document.getElementById("intro-tagline");
+
+const typewriter = new Typewriter(introTagline, {
+  loop: true,
+  delay: 70,
+  deleteSpeed: 20,
+});
+
+const initialDelay = 600;
+const waitTime = 1000;
+const typeDelay = 500;
+
+typewriter
+  .pauseFor(initialDelay)
+
+  .typeString("experiences.")
+  .pauseFor(waitTime)
+  .deleteChars(12)
+  .pauseFor(typeDelay)
+
+  .typeString("websites.")
+  .pauseFor(waitTime)
+  .deleteChars(9)
+  .pauseFor(typeDelay)
+
+  .typeString("applications.")
+  .pauseFor(waitTime)
+  .deleteChars(13)
+  .pauseFor(typeDelay)
+
+  .typeString("solutions.")
+  .pauseFor(waitTime)
+  .deleteChars(10)
+  .pauseFor(typeDelay)
+
+  .start();
