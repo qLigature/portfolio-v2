a6d20dd9589759d2c902d5c373140f78b4f2b335
qLigature <imbrued.mitre@gmail.com>
2022-05-04 02:46:58 +0800

    slight improvement to card and modal links

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,6 +1,13 @@
 const cards = document.getElementsByClassName("project-card");
 
 Array.from(cards).forEach((card) => {
+  // prevent overlapping behavior with clicks when clicking on demo
+  const demoLink = card.querySelector(".project-demo");
+  const sourceLink = card.querySelector(".project-source");
+
+  demoLink.addEventListener("click", (e) => e.stopPropagation());
+  sourceLink.addEventListener("click", (e) => e.stopPropagation());
+
   // add event listener to each card
   const modalId = card.getAttribute("data-target");
   const modal = document.getElementById(modalId);
@@ -8,8 +15,8 @@ Array.from(cards).forEach((card) => {
 
   const fadeOutDelay = 280;
 
+  // Open modal by clicking on card
   card.addEventListener("click", () => {
-    // Open modal by clicking on card
     modal.classList.remove("modal-fadeout");
     modal.showModal();
     modal.style.display = "block";
