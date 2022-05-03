83282a63df91c3e75cf0efdee7441feb2ae2c175
qLigature <imbrued.mitre@gmail.com>
2022-05-03 22:17:03 +0800

    added fadeout for closing modals

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -4,36 +4,41 @@ Array.from(cards).forEach((card) => {
   // add event listener to each card
   const modalId = card.getAttribute("data-target");
   const modal = document.getElementById(modalId);
-  // const closeBtn = modal.querySelector(".modal-close");
   const closeIcon = modal.querySelector(".custom-modal-close");
-  console.log(closeIcon);
+
+  const fadeOutDelay = 280;
 
   card.addEventListener("click", () => {
-    // open modal
+    // Open modal by clicking on card
+    modal.classList.remove("modal-fadeout");
     modal.showModal();
     modal.style.display = "block";
   });
 
+  // Close via clicking X icon on modal header
+  closeIcon.addEventListener("click", () => {
+    modal.classList.add("modal-fadeout");
+    setTimeout(() => {
+      modal.close();
+      modal.style.display = "none";
+    }, fadeOutDelay);
+  });
+
+  // Close via clicking outside modal
+  modal.addEventListener("click", function (e) {
+    if (!e.target.closest(".custom-modal-content")) {
+      modal.classList.add("modal-fadeout");
+      setTimeout(() => {
+        modal.close();
+        modal.style.display = "none";
+      }, fadeOutDelay);
+    }
+  });
+
+  // Esc key - quick close
   document.body.addEventListener("keydown", (e) => {
     if (e.key !== "Escape") return;
     modal.close();
     modal.style.display = "none";
-  });
-
-  closeIcon.addEventListener("click", () => {
-    modal.close();
-    modal.style.display = "none";
-  });
-
-  // closeBtn.addEventListener("click", () => {
-  //   modal.close();
-  //   modal.style.display = "none";
-  // });
-
-  document.querySelector("dialog").addEventListener("click", function (e) {
-    if (!e.target.closest("div")) {
-      e.target.close();
-      modal.style.display = "none";
-    }
   });
 });
