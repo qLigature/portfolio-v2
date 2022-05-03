206066b48195f9dbdfd03239e1bded1a0f5adc43
qLigature <imbrued.mitre@gmail.com>
2022-05-03 18:39:39 +0800

    added test modals

---
+++ resources/js/modal.js
@@ -0,0 +1,26 @@
+const cards = document.getElementsByClassName("project-card");
+
+Array.from(cards).forEach((card) => {
+  // add event listener to each card
+  const modalId = card.getAttribute("data-target");
+  const modal = document.getElementById(modalId);
+  const closeBtn = modal.querySelector(".modal-close");
+  console.log(closeBtn);
+
+  card.addEventListener("click", () => {
+    // open modal
+    modal.showModal();
+    modal.style.display = "block";
+  });
+
+  document.body.addEventListener("keydown", (e) => {
+    if (e.key !== "Escape") return;
+    modal.close();
+    modal.style.display = "none";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.close();
+    modal.style.display = "none";
+  });
+});
